6baa181f80a7049144c7d5c7b292422b63a55396
johan-smc <johanmurillo@Johans-MacBook-Pro.local>
2016-07-24 04:29:13 -0500

    416 complete

--- 416-1.cpp
+++ 416-1.cpp
@@ -16,31 +16,45 @@ bool arr[10][7]=
   {1,1,1,1,1,1,1},{1,1,1,1,0,1,1}
 };
 
-int pos(vector<bool> v, int n )
+int pos(vector<bool> v, int n ,vector<bool>yes)
 {
   foi( i , 0 , 7 )
   {
-    if(v[i] && !arr[n][i] )
+    if( yes[i]  && v[i]!=arr[n][i])
+      return -1;
+    if((v[i] && !arr[n][i] ))
       return -1;
   }
   return n;
 }
 
-bool funcion(vector< vector<bool> > &v,int row,int nmin,vector<bool> pas)
+void  tru(vector<bool>&v,int n, vector<bool>&yes){
+  foi( i , 0 , 7 )
+  {
+    if( v[i] )
+      yes[i]=true;
+  }
+}
+
+bool funcion(vector< vector<bool> > &v,int row,int nmin,vector<bool> pas,vector<bool> yes)
 {
+  // cout<<row<<" "<<nmin<<endl;
   if(row==-1)
   {
     return true;
   }
+  if( nmin == 9 )
+    return false;
   if( nmin==-1)
   {
     foi( i , 0 , 10 )
     {
-      int n = pos(v[row],i);
+      int n = pos(v[row],i,yes);
       if( n>=0)
       {
         pas[n]=true;
-        if(funcion(v,row-1,n,pas) )
+        tru(v[row],n,yes);
+        if(funcion(v,row-1,n,pas,yes) )
           return true;
         pas[n]=false;
       }
@@ -49,15 +63,16 @@ bool funcion(vector< vector<bool> > &v,int row,int nmin,vector<bool> pas)
   }
   else
   {
-    int n = pos(v[row],nmin+1);
+    int n = pos(v[row],nmin+1,yes);
     bool p=true;
     if( n!=-1)
     {
       p=pas[n];
       if(!pas[n])
         pas[n]=true;
+        tru(v[row],n,yes);
       }
-    if( n>=0  && !p &&funcion(v,row-1,n,pas))
+    if( n>=0  && !p &&funcion(v,row-1,n,pas,yes))
     {
 
        return true;
@@ -73,7 +88,7 @@ int main()
   while( scanf("%d",&n)!=EOF && n!=0)
   {
     vector< vector < bool > > v(n,vector<bool>(7,false));
-    vector< bool > pas(12,false);
+    vector< bool > pas(12,false),yes(7,false);
     string pal;
     foi( i , 0 , n )
     {
@@ -86,7 +101,7 @@ int main()
           v[i][j]=false;
       }
     }
-    if(funcion( v, n-1,-1 ,pas ))
+    if(funcion( v, n-1,-1 ,pas ,yes))
       printf("MATCH\n");
     else
       printf("MISMATCH\n");
