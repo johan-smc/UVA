a245e55d13a5a001661c132e0810315334116888
Johan <johan_smc@hotmail.com>
2016-08-02 17:25:59 -0500

    183 almost complete

--- 183.cpp
+++ 183.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <cstdio>
 #include <vector>
+#include <string>
 
 #define foi( i , n , k ) for( int i =  n ; i <  k ; ++i )
+using namespace std;
 
-string imprimir(vector< vector<bool> > &mat , int xi, int yi ,int xf,int yf)
+pair<bool,bool> revisar(vector< vector<bool> > &mat, int xi, int yi ,int xf,int yf)
+{
+  if (xi>xf||yi>yf) {
+    return make_pair(false,false);
+  }
+  pair<int,int> res;
+  res.second=mat[xi][yi];
+  for( int  i = xi ; i <= xf ; ++i  )
+  {
+    for ( int j =  yi ; j < yf ; ++j )
+    {
+      if(res.second!=mat[i][j])
+      {
+        res.first=false;
+        return res;
+      }
+    }
+  }
+  res.first=true;
+  return res;
+}
+
+string  imprimir(vector< vector<bool> > &mat , int xi, int yi ,int xf,int yf)
 {
   if (xi>xf||yi>yf) {
     return "";
   }
   string res="";
-  pair<bool,bool> re=revisar(mat);
+  pair<bool,bool> re=revisar(mat,xi,yi,xf,yf);
   if( re.first )
     return res+='0'+(int)re.second;
   res+='D';
@@ -27,11 +51,22 @@ void llenar(const string &m,int &n,std::vector<std::vector<bool> > &mat, int xi,
     return;
   if(m[n]=='D')
   {
-
+    ++n;
+    llenar(m,n,mat,xi,yi,(xi+xf)/2,(yi+yf)/2);
+    llenar(m,n,mat,xi,(yi+yf)/2+1,(xi+xf)/2,yf);
+    llenar(m,n,mat,(xi+xf)/2+1,yi,xf,(yi+yf)/2);
+    llenar(m,n,mat,(xi+xf)/2+1,(yi+yf)/2+1,xf,yf);
   }
   else
   {
-    foi( i = xi ; i <  )
+    for( int  i = xi ; i <= xf ; ++i  )
+    {
+      for ( int j =  yi ; j < yf ; ++j )
+      {
+        mat[i][j]=m[n];
+      }
+    }
+    ++n;
   }
 }
 
@@ -54,13 +89,14 @@ int main()
           scanf("%d",&temp);
           mat[i][j]=temp;
         }
-        cout<<imprimir(mat)<<endl;;
+        cout<<imprimir(mat,0,0,x-1,y-1)<<endl;;
     }
     else if(c=='D')
     {
       string m;
       cin>>m;
-      llenar(m,mat);
+      int pos=0;
+      llenar(m,pos,mat,0,0,x-1,y-1);
       foi( i , 0 , x)
         foi ( j , 0 , y )
           printf("%d",(bool)mat[i][j]);
