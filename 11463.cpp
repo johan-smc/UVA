e2e1acac262bef5bfed801036a9762ac522a2066
johan-smc <johan_smc@hotmail.com>
2017-07-04 20:36:15 -0500

    not complete

--- 11463.cpp
+++ 11463.cpp
@@ -7,12 +7,16 @@
 
 using namespace std;
 #define MAX 105
+#define INF 1<<20
 
 typedef pair< int , int > ii;
 typedef pair< int , ii > iii;
-
+bitset< MAX > visited;
 int graph[ MAX ][ MAX ];
-
+int path[ MAX ][ MAX ];
+int ff[ MAX ];
+vector< int > res;
+bool fres;
 int n,r;
 
 void floyd()
@@ -26,14 +30,23 @@ void floyd()
         if( graph[ i ][ j ] > dt )
         {
           graph[ i ][ j ] = dt ;
-          path[ i ][ j ] = path[ k ][ j ];
+          path[ i ][ j ] = path[ i ][ k ];
         }
       }
 }
 
-void re( int i , int j )
+int re( int i , int j )
 {
+  // cout << i << " " << j << " " << visited[ i ]<< " " << visited[ j ] << endl;
+  if( visited[ i ] )
+    return i;
+  int s = i;
+  if( i != j ) s = re( path[ i ][ j ] , j );
+  if( fres )
+    res.push_back( i );
 
+  visited[ i ]  = 1;
+  return s;
 }
 
 int main()
@@ -44,6 +57,7 @@ int main()
   scanf("%d",&t);
   while( t-- )
   {
+    memset( ff , -1 , sizeof ff );
     scanf("%d %d",&n ,&r);
     for( int i = 0 ; i < n ; ++i )
       for( int j = 0 ; j < n ; ++j )
@@ -52,7 +66,7 @@ int main()
           graph[ i ][ j ] = 0;
         else
           graph[ i ][ j ] = INF;
-        path[ i ][ j ] = i;
+        path[ i ][ j ] = j;
       }
     for( int  i = 0 ; i < r  ; ++i )
     {
@@ -61,6 +75,58 @@ int main()
       graph[ b ][ a ] = min(1,graph[b][a]);
     }
     scanf("%d %d",&a,&b );
+    floyd();
+    visited.reset();
+    int totW = 0;
+    // cout << a << " " << b << " " << graph[ a ][ b ] << endl;
+    totW += graph[ a ][ b ];
+    fres = true;
+    res.clear();
+    re( a , b  );
+    fres = false;
+    /*
+    // cout << a <<  " "<< b << endl;
+    // cout << "hola" << endl;
+    int may = -1, tt = 0;
+    for( int i = 0 ; i < n ; ++i )
+    {
+      temp = i;
+      if( ! visited[ i ] )
+      {
+
+        temp = re( i , b );
+      // cout << temp << endl;
+      // totW += 2*graph[ i ][ temp ];
+      // ff[ temp ] = max( ff[ temp ] , 2*graph[ i ][ temp ] );
+      // may = max( may , graph[ i ][ temp ] + min(graph[ i ][ temp ] , graph[ i ][ b ] ) );
+        tt = graph[ i ][ b ] - graph[ temp ][ b ];
+        if ( tt < 0 )
+          tt = 0;
+        cout << "hk "<< tt << " i " << i  << " " << graph[ i ][ b ] << " " << graph[ temp ][ b ] << " temp " << temp<< endl;
+        may = max( may , graph[ i ][ temp ] + ( min ( graph[ i ][ temp ] , tt )));
+      }
+      // cout << totW << endl;
+    }
+    */
+    int may = 0, temp = 0  ;
+    for( int i = 0; i < n ; ++i )
+    {
+      if( !visited[ i ] )
+      {
+        temp = INF;
+        for( int j = 0 ; j < res.size() ; ++j )
+        {
+          int tt ;
+          tt = graph[ i ][ b ] - graph[ res[ j ] ][ b ];
+          if( tt < 0 )
+            tt = 0;
+          temp = min( temp , graph[ i ][ res[ j ] ] + min( graph[ i ][ res [ j ] ] , tt ));
+        }
+        may = max( may , temp );
+      }
+      // cout << "llegue" << endl;
+    }
+    totW += may;
     printf("Case %d: %d\n",++c,totW );
   }
   return 0;
